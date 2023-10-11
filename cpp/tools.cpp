c170d5f4e2493aa7be1efcb93de9b3025510de9e
sherlock55341 <1398660594@qq.com>
2023-10-11 22:33:28 +0800

    Add Batch Generate Tool

--- cpp/tools.cpp
+++ cpp/tools.cpp
@@ -1,44 +1,98 @@
 #include "tools.hpp"
+#include <boost/geometry.hpp>
+#include <boost/geometry/index/rtree.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/geometries/point.hpp>
+
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+using bp = bg::model::point<int, 2, bg::cs::cartesian>;
+using bb = bg::model::box<bp>;
+using rt = bgi::rtree<std::pair<bb, int>, bgi::rstar<32>>;
 
 UTILS_BEGIN
 
 tools::Timer::Timer() : tp(std::chrono::high_resolution_clock::now()) {}
 
-void tools::Timer::reset(){
-    tp = std::chrono::high_resolution_clock::now();
-}
+void tools::Timer::reset() { tp = std::chrono::high_resolution_clock::now(); }
 
-double tools::Timer::elapse() const{
-    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - tp).count() / 1e6;
+double tools::Timer::elapse() const {
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+               std::chrono::high_resolution_clock::now() - tp)
+               .count() /
+           1e6;
 }
 
 tools::LogWithTimer::LogWithTimer(int _verbose) : verbose_(_verbose) {}
 
-void tools::LogWithTimer::reset(){
-    timer_.reset();
-}
+void tools::LogWithTimer::reset() { timer_.reset(); }
 
-void tools::LogWithTimer::setVerbose(int _verbose){
-    verbose_ = _verbose;
-}
+void tools::LogWithTimer::setVerbose(int _verbose) { verbose_ = _verbose; }
 
-void tools::LogWithTimer::operator()(const char* __restrict__ format) const {
-    if(verbose_ == 0){
-        return ;
+void tools::LogWithTimer::operator()(const char *__restrict__ format) const {
+    if (verbose_ == 0) {
+        return;
     }
     printf("[%7.3f s]", timer_.elapse());
     printf(format);
     fflush(stdout);
 }
 
-void tools::LogWithTimer::operator()(bool enable, const char* __restrict__ format) const {
-    if(verbose_ == 0 || enable == 0){
-        return ;
+void tools::LogWithTimer::operator()(bool enable,
+                                     const char *__restrict__ format) const {
+    if (verbose_ == 0 || enable == 0) {
+        return;
     }
     printf("[%7.3f s]", timer_.elapse());
     printf(format);
     fflush(stdout);
 }
 
+static void insert(rt &rtree, const geo::RectT<int> &box, int target) {
+    bb bbox(bp(box.lx(), box.ly()), bp(box.hx(), box.hy()));
+    rtree.insert({bbox, target});
+}
+
+static bool hasConflict(rt &rtree, const geo::RectT<int> &box, int target) {
+    bb bbox(bp(box.lx(), box.ly()), bp(box.hx(), box.hy()));
+    std::vector<std::pair<bb, int>> results;
+    rtree.query(bgi::intersects(bbox), std::back_inserter(results));
+    for (const auto &result : results) {
+        if (result.second != target) {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::vector<std::vector<int>>
+tools::BatchGenerator::gen(const std::vector<geo::RectT<int>> &boxes,
+                           const std::vector<int> &targets, int maxBatch,
+                           int maxPace) {
+    std::vector<std::vector<int>> batches;
+    rt rtree;
+    std::vector<bool> chosen(targets.size(), false);
+    int last = 0;
+    while (last < targets.size()) {
+        rtree.clear();
+        batches.emplace_back();
+        auto &batch = batches.back();
+        for (int i = last; i < targets.size(); i++) {
+            if (batch.size() == maxBatch || i - last == maxPace)
+                break;
+            if (chosen[i])
+                continue;
+            if (!hasConflict(rtree, boxes[targets[i]], targets[i])) {
+                insert(rtree, boxes[targets[i]], targets[i]);
+                batch.emplace_back(targets[i]);
+                chosen[i] = true;
+            }
+        }
+        while (last < targets.size() && chosen[last]) {
+            last++;
+        }
+    }
+    return batches;
+}
 
 UTILS_END
--- cpp/tools.hpp
+++ cpp/tools.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "geo.hpp"
 #include <chrono>
 
 #include "../global.hpp"
@@ -8,54 +9,62 @@ UTILS_BEGIN
 
 namespace tools {
 class Timer {
- private:
-  std::chrono::time_point<std::chrono::high_resolution_clock> tp;
+  private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> tp;
 
- public:
-  Timer();
+  public:
+    Timer();
 
-  void reset();
+    void reset();
 
-  double elapse() const;
+    double elapse() const;
 };
 
 class LogWithTimer {
- private:
-  Timer timer_;
-  int verbose_;
+  private:
+    Timer timer_;
+    int verbose_;
 
- public:
+  public:
     LogWithTimer(int _verbose = 1);
 
     void reset();
 
     void setVerbose(int _verbose);
 
-    template<typename ...Args>
-    void operator()(const char* __restrict__ format, Args&&... args) const{
-        if(verbose_ == 0){
-            return ;
+    template <typename... Args>
+    void operator()(const char *__restrict__ format, Args &&...args) const {
+        if (verbose_ == 0) {
+            return;
         }
         printf("[%7.3f s]", timer_.elapse());
         printf(format, args...);
         fflush(stdout);
     }
 
-    void operator()(const char* __restrict__ format) const;
+    void operator()(const char *__restrict__ format) const;
 
-    template<typename ...Args>
-    void operator()(bool enable, const char* __restrict__ format, Args&&... args) const{
-        if(verbose_ == 0 || enable == 0){
-            return ;
+    template <typename... Args>
+    void operator()(bool enable, const char *__restrict__ format,
+                    Args &&...args) const {
+        if (verbose_ == 0 || enable == 0) {
+            return;
         }
         printf("[%7.3f s]", timer_.elapse());
         printf(format, args...);
         fflush(stdout);
     }
 
-    void operator()(bool enable, const char* __restrict__ format) const;
-
+    void operator()(bool enable, const char *__restrict__ format) const;
 };
-}  // namespace tools
+
+class BatchGenerator {
+  public:
+
+    static std::vector<std::vector<int>> gen(const std::vector<geo::RectT<int>> &boxes,
+                                             const std::vector<int> &targets,
+                                             int maxBatch, int maxPace);
+};
+} // namespace tools
 
 UTILS_END
