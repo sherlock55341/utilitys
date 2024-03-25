381363c2c2e02defa82de92c9aab3078be36db44
sherlock55341 <1398660594@qq.com>
2024-03-25 13:24:47 +0800

    add fill

--- cuda/tools.cuh
+++ cuda/tools.cuh
@@ -4,17 +4,28 @@
 
 UTILS_BEGIN
 
+namespace cuda::kernels {
+template <typename T, typename U>
+__global__ void fill(T *arr, U val, size_t N) {
+    auto idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx < N) {
+        arr[idx] = val;
+    }
+}
+} // namespace cuda::kernels
 namespace cuda {
 
-template <typename T> 
+template <typename T>
 __forceinline__ __device__ __host__ void swap(T &x, T &y) {
     T t(x);
     x = y;
     y = t;
 }
 
-__forceinline__ __device__ size_t naiveIdx(){
-    return threadIdx.x + blockIdx.x * blockDim.x;
+template <typename T, typename U>
+void fill(T *arr, U val, size_t N, int THREADS_PER_BLOCK) {
+    kernels::fill<<<(N + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK,
+                    THREADS_PER_BLOCK>>>(arr, val, N);
 }
 } // namespace cuda
 
